0a06cb3909eff318f2bff84ce864e33c37483e19
Léo Jason Coulais <leo.jascoulais@gmail.com>
2026-03-12 11:05:06 +0100

    [+] Adding android development kit

--- android.nix
+++ android.nix
@@ -6,7 +6,7 @@
   programs.adb.enable = true;
   users.users.nit.extraGroups = ["adbusers"];
 
-  environment.systemPackages = [ pkgs.android-studio ];
+  environment.systemPackages = [ pkgs.android-tools pkgs.android-studio ];
 
 }
 
